9ce609f133c509b32bd83f481117400f05d99957
reda mjibel <reda_mjibel@hotmail.com>
2024-03-21 22:10:11 +0000

    test glam

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- bands with Glam rock as their main style, ranked by their longevity
+
+SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
+FROM metal_bands
+WHERE style like '%Glam rock%';
