0a45e79b495bf07fe1677c8ca75bfd9215bb1afd
reda mjibel <reda_mjibel@hotmail.com>
2024-03-22 02:22:59 +0000

    test view

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,7 @@
+-- lists all students that have a score under 80
+
+DROP VIEW IF EXISTS need_meeting;
+
+CREATE VIEW need_meeting AS
+SELECT name FROM students
+WHERE score < 80 AND last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH);
