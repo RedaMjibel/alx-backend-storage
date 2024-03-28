64a05caecab2cef674577e0a92541a4a9cb5912d
Reda Mjibel <reda_mjibel@hotmail.com>
2024-03-28 02:57:26 +0000

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,38 +1,31 @@
-#!/usr/bin/env python3
-
-"""
-Writing strings to Redis
-"""
-import redis
-import uuid
-from typing import Union, Callable
-
-
-
 class Cache:
-    def __init__(self):
+    '''Writing strings to Redis'''
+    def __init__(self) -> None:
+        '''Initializes a Cache instance and flushes the Redis database.'''
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-	"""Stores a value in Redis and returns the key. """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        '''Stores a value in Redis and returns the key.'''
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
-	"""Retrieves a value from Redis with optional transformation."""
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        '''Retrieves a value from Redis with optional transformation.'''
         data = self._redis.get(key)
-        if data is None:
-            return None
-        if fn:
-            return fn(data)
-        return data
+        return fn(data) if fn is not None else data
 
-    def get_str(self, key: str) -> Union[str, bytes]:
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
+    def get_str(self, key: str) -> str:
+        '''Retrieves a string value from Redis.'''
+        return self.get(key, lambda x: x.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
-        return self.get(key, fn=int)
+        '''Retrieves an integer value from Redis.'''
+        return self.get(key, lambda x: int(x))
