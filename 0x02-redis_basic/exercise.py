212139ba3d7c54c110b8d7f6adc599a482172315
Reda Mjibel <reda_mjibel@hotmail.com>
2024-03-28 02:54:57 +0000

    test get

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,16 +5,32 @@ Writing strings to Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
+
 
 
 class Cache:
     def __init__(self):
-        """Stores a value in Redis and returns the key. """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+	"""Stores a value in Redis and returns the key. """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+	"""Retrieves a value from Redis with optional transformation."""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, bytes]:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        return self.get(key, fn=int)
