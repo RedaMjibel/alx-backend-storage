4b027d0694054ee7aa1f98d6825d0033b3f60729
Reda Mjibel <reda_mjibel@hotmail.com>
2024-03-28 02:58:33 +0000

    incr

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,64 @@
+#!/usr/bin/env python3
+'''Redis NoSQL data storage module with method call tracking.'''
+import uuid
+import redis
+from functools import wraps
+from typing import Any, Callable, Union
+
+
+def count_calls(method: Callable) -> Callable:
+    '''Decorator to count method calls in a Cache class.'''
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        '''Increments method call counter and invokes the original method.'''
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return invoker
+
+
+def call_history(method: Callable) -> Callable:
+    '''Decorator to track call details of methods in a Cache class.'''
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        '''Stores method inputs and outputs and returns the output.'''
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}:outputs'.format(method.__qualname__)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(out_key, output)
+        return output
+    return invoker
+
+
+def replay(fn: Callable) -> None:
+    '''Displays method call history of a Cache class.'''
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fxn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fxn_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
+        ))
+
+
 class Cache:
-    '''Writing strings to Redis'''
+    '''An object for storing data in Redis with call tracking.'''
     def __init__(self) -> None:
         '''Initializes a Cache instance and flushes the Redis database.'''
         self._redis = redis.Redis()
